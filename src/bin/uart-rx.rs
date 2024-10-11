0a7b11fbf03425aa6fbbc566ce32dfd4801957a6
flippette <me@flippo.cc>
2024-10-11 03:36:07 +0300

    new uart-{tx,rx} implementation (doesn't link yet)

--- src/bin/uart-rx.rs
+++ src/bin/uart-rx.rs
@@ -1,11 +1,9 @@
 #![no_std]
 #![no_main]
 
-use core::convert::Infallible;
+use core::{convert::Infallible, iter, str};
 
-use arduino_hal::{
-    default_serial, delay_ms, entry, pins, prelude::*, Peripherals,
-};
+use arduino_hal::{default_serial, entry, pins, prelude::*, Peripherals};
 use heapless::Vec;
 use nb::block;
 use panic_halt as _;
@@ -23,30 +21,46 @@ fn main() -> Result<(), Infallible> {
     // SAFETY: we only run this once on init
     let peri = unsafe { Peripherals::steal() };
     let pins = pins!(peri);
+
     let mut serial = default_serial!(peri, pins, 115_200);
-    let mut buf = Vec::<u8, 32>::new();
-    let mut led = pins.d13.into_output();
-    let mut counter = 0;
+    let mut cur_id = 0;
 
     loop {
-        buf.clear();
-        loop {
-            match block!(serial.read())? {
-                b'\n' => break,
-                ch => buf.push(ch)?,
+        // read an entire line, up to 32 bytes
+        let msg = iter::repeat_with(|| block!(serial.read()))
+            .map_while(Result::ok)
+            .take_while(|b| *b != b'\n')
+            .take(32)
+            .collect::<Vec<_, 32>>();
+
+        let mut parts = msg.split(|b| *b == b' ');
+
+        let Some(kind) = parts
+            .next()
+            .filter(|s| [&b"send"[..], &b"sync"[..]].contains(s))
+        else {
+            // invalid request, try resync
+            uwriteln!(serial, "sync {}", cur_id)?;
+            continue;
+        };
+
+        match kind {
+            b"send" => {
+                uwriteln!(serial, "recv {}", cur_id)?;
+                cur_id += 1;
             }
+            b"sync" => {
+                if let Some(id) = parts
+                    .next()
+                    .and_then(|s| str::from_utf8(s).ok())
+                    .and_then(|s| s.parse::<u32>().ok())
+                {
+                    cur_id = id;
+                }
+
+                uwriteln!(serial, "sync {}", cur_id)?;
+            }
+            _ => unreachable!(),
         }
-
-        if buf.split(u8::is_ascii_whitespace).next() != Some(b"ping") {
-            break;
-        }
-
-        delay_ms(1_000);
-
-        uwriteln!(serial, "pong {}", counter)?;
-        counter += 1;
     }
-
-    led.set_high();
-    panic!();
 }
--- src/bin/uart-tx.rs
+++ src/bin/uart-tx.rs
@@ -1,11 +1,9 @@
 #![no_std]
 #![no_main]
 
-use core::convert::Infallible;
+use core::{convert::Infallible, iter, str};
 
-use arduino_hal::{
-    default_serial, delay_ms, entry, pins, prelude::*, Peripherals,
-};
+use arduino_hal::{default_serial, entry, pins, prelude::*, Peripherals};
 use heapless::Vec;
 use nb::block;
 use panic_halt as _;
@@ -23,32 +21,56 @@ fn main() -> Result<(), Infallible> {
     // SAFETY: we only run this once on init
     let peri = unsafe { Peripherals::steal() };
     let pins = pins!(peri);
-    let mut serial = default_serial!(peri, pins, 115_200);
-    let mut buf = Vec::<u8, 32>::new();
-    let mut led = pins.d13.into_output();
-    let mut counter = 0;
 
-    delay_ms(1_000);
+    let mut serial = default_serial!(peri, pins, 115_200);
+    let mut cur_id = 0;
+
+    // trig must be pulled down to GND
+    let trig = pins.d2;
+    let mut held = false;
 
     loop {
-        uwriteln!(serial, "ping {}", counter)?;
-        counter += 1;
+        match trig.is_high() {
+            true if !held => {
+                held = true;
 
-        buf.clear();
-        loop {
-            match block!(serial.read())? {
-                b'\n' => break,
-                ch => buf.push(ch).unwrap(),
+                // send the initial ping
+                uwriteln!(serial, "send {}", cur_id)?;
+
+                // verify and sync
+                loop {
+                    // read an entire line, up to 32 bytes
+                    let msg = iter::repeat_with(|| block!(serial.read()))
+                        .map_while(Result::ok)
+                        .take_while(|b| *b != b'\n')
+                        .take(32)
+                        .collect::<Vec<_, 32>>();
+
+                    let mut parts = msg.split(|b| *b == b' ');
+                    if parts
+                        .next()
+                        .filter(|s| [&b"recv"[..], &b"sync"[..]].contains(s))
+                        .zip(
+                            parts
+                                .next()
+                                .and_then(|s| str::from_utf8(s).ok())
+                                .and_then(|s| s.parse::<u32>().ok())
+                                .filter(|id| *id == cur_id),
+                        )
+                        .is_none()
+                    {
+                        // malformed message, try resync
+                        uwriteln!(serial, "sync {}", cur_id)?;
+                        continue;
+                    };
+
+                    break;
+                }
+
+                cur_id += 1;
             }
+            false => held = false,
+            _ => {}
         }
-
-        if buf.split(u8::is_ascii_whitespace).next() != Some(b"pong") {
-            break;
-        }
-
-        delay_ms(1_000);
     }
-
-    led.set_high();
-    panic!();
 }
